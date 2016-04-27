ad57a7c9dc9bac592ff143dfa650968ec217a9d2
Grant Slape <notrilia.republic@gmail.com>
2016-04-27 18:49:35 -0500

    Skeleton
    Skeleton done, algorithms and documentation to come.

---
+++ heapdriver_slape.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+void heapify(int[], int size);
+/*
+	Precondition:
+	Postcondition:
+*/
+
+void percolateDown(int[], int *, int size);
+/*
+	Precondition:
+	Postcondition:
+*/
+
+void displayArray(int[], int size);
+/*
+	Displays the contents of the array in a horizontal fashion.
+	Precondition: None
+	Postcondtion: the contents of the array are outputted to cout
+*/
+
+void displayTree();
+/*
+	Precondition:
+	Postcondition:
+*/
+
+void heapsort(int[], int size);
+/*
+	Precondition:
+	Postcondition:
+*/
+
+int main() {
+  // Repeat until user is ready to quit
+  //  Input the size of the array from the user
+  //  Dynamically allocate an array of integers of the size given
+  //  (+ 1 since element 0 is reserved)
+  //  Populate the array with random numbers in the range 0 thru 999
+  //  Call function displayTree
+  //  Call function displayArray
+  //  Input choice of either [h]eapify or heap[s]ort from the user
+  //  If choice is heapify
+  //  Call function heapify
+  //  Else
+  //  Call function heapsort
+  //  EndIf
+  //  Call function displayTree
+  //  Call function displayArray
+  //  Free the array
+  //  End repeat
+	return 0;
+}
